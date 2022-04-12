806546314bf829f5947dcd5b8f890b50337321bd
jaeho-jang <lemphis@gmail.com>
2022-04-12 13:46:56 +0900

    Update initialize.sh

--- init/initialize.sh
+++ init/initialize.sh
@@ -7,7 +7,10 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+brew tap homebrew/cask-versions
+
 brew install --cask \
+    temurin \
     google-chrome \
     iterm2 \
     slack \
@@ -15,16 +18,23 @@ brew install --cask \
     intellij-idea
 
 brew install \
+    kotlin \
     zsh-autosuggestions \
     zsh-fast-syntax-highlighting
-    openjdk \
     maven \
     gradle \
-    node
+    nvm
+
+mkdir ~/.nvm
 
 # ~/.zshrc에 아래 구문 입력
 # source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 # source $(brew --prefix)/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
+# [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"
 
 # 입력 후 아래 command 실행
 # source ~/.zshrc
+
+# nvm install --lts
